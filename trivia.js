e519da24ff5e86b8a348371828be4db92ef6d95b
keshiki808 <keshiki808@gmail.com>
2021-07-23 22:13:27 -0230

    Initial javascript work

---
+++ trivia.js
@@ -0,0 +1,13 @@
+const $ = (selector) => document.querySelector(selector);
+
+const aChoice = $('#a-radio');
+
+const bChoice = $('b-radio');
+
+const cChoice = $('#c-radio');
+
+const dChoice = $('#d-radio');
+
+document.addEventListener('DOMContentLoaded', () => {
+
+});
